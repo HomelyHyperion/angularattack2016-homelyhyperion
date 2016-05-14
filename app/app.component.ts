a769665256766a1e74c5bc14c4a9e55c0e710a19
Victor Cheong <victor.cheong@tower.co.nz>
2016-05-14 20:46:41 +1200

    Add HeaderComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
+    directives: [HeaderComponent]
 })
 
 export class AppComponent {
