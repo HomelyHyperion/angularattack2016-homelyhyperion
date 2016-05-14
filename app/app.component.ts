6a19b18a372f8dbcf74f2bb2a31a125b872dbdbe
Victor Cheong <victor.cheong@tower.co.nz>
2016-05-15 01:39:46 +1200

    Add TaskListComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { TaskFormComponent } from './task-form.component';
+import { TaskListComponent } from './task-list.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    directives: [HeaderComponent, TaskFormComponent]
+    directives: [HeaderComponent, TaskFormComponent, TaskListComponent]
 })
 
 export class AppComponent {
---
+++ app/task-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'task-list',
+    templateUrl: 'app/task-list.component.html',
+    styleUrls: ['app/task-list.component.css']
+})
+export class TaskListComponent {}
