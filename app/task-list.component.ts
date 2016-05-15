b1783817cba0dbcc4794ceace7be3b6a84135e51
Victor Cheong <victor.cheong@tower.co.nz>
2016-05-15 15:30:34 +1200

    Bind tasks to TaskListComponent

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from './task';
 
 @Component({
     selector: 'task-list',
     templateUrl: 'app/task-list.component.html',
     styleUrls: ['app/task-list.component.css']
 })
-export class TaskListComponent {}
+export class TaskListComponent {
+    @Input() tasks: Task[];
+}
